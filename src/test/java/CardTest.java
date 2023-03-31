9f5725425da4accaca9448257db8fa3a0245edca
Andrey Turchak <andrewturchak78@mail.ru>
2023-03-31 18:43:50 +0300

    test fixed

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -7,7 +7,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import static com.codeborne.selenide.Condition.*;
@@ -16,10 +19,15 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardTest {
     @Test
     void test() {
-        Configuration.headless = true ;
+        LocalDate deliveryDate = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDate.format(formatter);
+        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id=date] input").setValue("19.04.2023");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(dateText);
+        //$("[data-test-id=date] input").setValue("19.04.2023");
         $("[data-test-id='name'] input").setValue("Дмитрий Петров");
         $("[data-test-id=phone] input").setValue("+79543781674");
         $("[data-test-id=agreement]").click();
