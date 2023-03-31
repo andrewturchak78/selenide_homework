533eb55508f234851c8400c7c32851293f6edcf6
Andrey Turchak <andrewturchak78@mail.ru>
2023-03-31 17:31:54 +0300

    test added

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,12 +1,29 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.TimeZone;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
     @Test
     void test() {
         Configuration.headless = true ;
         open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Казань");
+        $("[data-test-id=date] input").setValue("19.04.2023");
+        $("[data-test-id='name'] input").setValue("Дмитрий Петров");
+        $("[data-test-id=phone] input").setValue("+79543781674");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
     }
 }
