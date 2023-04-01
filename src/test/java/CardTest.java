f07f084b2a701b6ca52675f7f0ef10c0a80dee15
Andrey Turchak <andrewturchak78@mail.ru>
2023-04-01 12:51:44 +0300

    test added

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -17,21 +17,28 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+        //LocalDate deliveryDate = LocalDate.now().plusDays(3);
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        //String dateText = deliveryDate.format(formatter);
+    }
     @Test
     void test() {
-        LocalDate deliveryDate = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDate.format(formatter);
-        Configuration.headless = true;
+        // Configuration.headless = true;
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Казань");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(dateText);
+        $("[data-test-id=date] input").setValue(planningDate);
         //$("[data-test-id=date] input").setValue("19.04.2023");
         $("[data-test-id='name'] input").setValue("Дмитрий Петров");
         $("[data-test-id=phone] input").setValue("+79543781674");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
